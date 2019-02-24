73a7dd217a8c7a2aff878fa0b737d9df9cfa6e36
Luis Guitron <legl_1995@hotmail.com>
2019-02-24 17:39:47 -0600

    Reduced code for inorder printing

--- Haskell/Hwk1/excercise4.hs
+++ Haskell/Hwk1/excercise4.hs
@@ -1,12 +1,12 @@
 --      Tree used
 --           5
 --        /     \
---       4       7
+--       3       7
 --      / \     /
---     2   3   6
+--     2   4   6
 --
 
-main = print (read_inorder(insertTree 6(insertTree 3(insertTree 2 (insertTree 7 (insertTree 4 (insertTree 5 EmptyTree)))))))
+main = print (read_inorder(insertTree 6(insertTree 4(insertTree 2 (insertTree 7 (insertTree 3 (insertTree 5 EmptyTree)))))))
 data Tree a = EmptyTree | Node a (Tree a)(Tree a) deriving (Show, Read, Eq)
 
 insertTree x EmptyTree = Node x (EmptyTree)(EmptyTree)
@@ -16,16 +16,7 @@ insertTree x (Node y left right)
            | x > y  = Node y left (insertTree x right)
            
 
-read_inorder (Node y left right) =
-
-    if left == EmptyTree
-    
-       -- No left tree
-       then if right == EmptyTree
-                then [y]
-                else [y] ++ (read_inorder right)
-        
-       -- Has left tree
-       else if right == EmptyTree
-                then (read_inorder left) ++ [y]
-                else (read_inorder left) ++ [y] ++ (read_inorder right)
+           
+           
+read_inorder EmptyTree = []
+read_inorder (Node y left right) = read_inorder(left) ++ [y] ++ read_inorder(right)
