ae70c9cdba78d2a44c1b6cedf570f060d501442b
Luis Guitron <legl_1995@hotmail.com>
2019-03-06 10:39:53 -0600

    Secant method with pattern matching

--- Haskell/ListComprehension/excercise2.hs
+++ Haskell/ListComprehension/excercise2.hs
@@ -1,13 +1,10 @@
 main = print(secant_method (\x -> x^3 + 2*x^2 +3*x -7) (-1) (1) 0.00001)
 
-secant_method :: (Float -> Float) -> Float -> Float -> Float -> Float
-secant_method f x0 x1 eps = 
-    
-    -- Calculate new value of x and compare epsilon values 
-    if abs (((x0 * f(x1) - x1 * f(x0))/(f(x1) - f(x0))) - x1) < eps
-        
-        -- return the root
-        then (x0 * f(x1) - x1 * f(x0))/(f(x1) - f(x0))
+secant_method :: (Floating a, Ord a) => (a -> a) -> a -> a -> a -> a
+secant_method f x0 x1 eps
 
-        -- update and continue
-        else secant_method f x1 ((x0 * f(x1) - x1 * f(x0))/(f(x1) - f(x0))) eps
+   | rootDifference < eps = x2
+   | otherwise = secant_method f x1 x2 eps
+   where
+       x2 = (x0 * f(x1) - x1 * f(x0))/(f(x1) - f(x0))
+       rootDifference = abs(x2-x1)
