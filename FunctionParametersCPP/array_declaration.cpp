bb71df9bf668dbd56bde29519eb78c1d0d33e32e
Luis Guitron <legl_1995@hotmail.com>
2019-03-20 09:53:07 -0600

    Reserve space for a pointer inside a function

--- FunctionParametersCPP/array_declaration.cpp
+++ FunctionParametersCPP/array_declaration.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include <cstdlib>
 
-void * declare_pointer(int size)
+template <class T>
+void reserve(T &pointer, int size)
 {
-   void *p = malloc(size);
-   return p;
+   pointer = (T) malloc(size*sizeof(T)); 
 }
+
 
 int main()
 {
     int p_sizes = 5;
     
-    // Declare int pointer
-    int* p1 = (int *) declare_pointer(p_sizes);
+    
+    // Int example
+    int *p1;
+    reserve(p1, p_sizes);
     p1[2] = 1;
-    
-    // Declare string pointer
-    char* p2 = (char* ) declare_pointer(p_sizes);
-    p2[0] = 'a';
-    p2[1] = 'b';
-    p2[2] = 'c';
-    p2[3] = 'd';
-    p2[4] = 'e';
-    
     for (int i = 0; i<p_sizes; i++)
     {
         std::cout<<p1[i]<< ", ";
     }
+    
     std::cout<<std::endl;
+    
+    // Char example
+    char *p2;
+    reserve(p2, p_sizes);
+    p2[3] = 'a';
     for (int i = 0; i<p_sizes; i++)
     {
         std::cout<<p2[i]<< ", ";
     }
-    std::cout<<std::endl;
 
     delete p1;
     delete p2;
-    return 1;
 }
