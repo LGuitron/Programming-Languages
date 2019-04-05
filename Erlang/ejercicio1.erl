ef12f5cbad00651df8c25b99797399109c87b022
Luis Guitron <legl_1995@hotmail.com>
2019-04-05 16:10:27 -0600

    Erlang threads with random sleep times

---
+++ Erlang/ejercicio1.erl
@@ -0,0 +1,18 @@
+-module(ejercicio1).
+
+-export([start/0, write_pid/3]).
+
+% Do nothing when there are no more calls remaining
+write_pid(Wait_Time, Call_id, 0) ->
+    done;
+
+write_pid(Wait_Time, Call_id, Calls_rem) ->
+    timer:sleep(Wait_Time),
+    io:fwrite("Proceso " ++ integer_to_list(Call_id) ++ " faltan " ++ integer_to_list(Calls_rem-1) ++"\n"),
+    write_pid(rand:uniform(10) * 10, Call_id, Calls_rem-1).
+
+start() ->
+    spawn(ejercicio1, write_pid, [rand:uniform(10) * 10, 0, 10]),
+    spawn(ejercicio1, write_pid, [rand:uniform(10) * 10, 1, 10]),
+    spawn(ejercicio1, write_pid, [rand:uniform(10) * 10, 2, 10]),
+    spawn(ejercicio1, write_pid, [rand:uniform(10) * 10, 3, 10]).
