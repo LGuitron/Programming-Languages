b51f24c7e850f45e90e3a8844d7e2b46d1178fc7
Luis Guitron <legl_1995@hotmail.com>
2019-04-07 12:15:34 -0500

    Receive result for row sums and store them in partial results

--- Erlang/ejercicio2.erl
+++ Erlang/ejercicio2.erl
@@ -1,148 +1,66 @@
 -module(ejercicio2).
 
--export([start/0, add_matrices/2, add_matrices_spawn/2 , add_matrices_receive/0, add_rows/4]).
-
-
-
+-export([start/0, add_matrices/2, spawn_sums/3, add_matrices_receive/3, add_rows/3]).
 
 
 
 add_matrices(Mat1, Mat2) ->
 
-    %spawn(ejercicio2, add_rows, [list_to_tuple(lists:nth(1, Mat1)), list_to_tuple(lists:nth(1, Mat2))]).
-    %io:format("~p~n",spawn(ejercicio2, add_rows, [list_to_tuple(lists:nth(1, Mat1)), list_to_tuple(lists:nth(1, Mat2))])).
+    % Register function for receiving process
+    register(receive_sum, spawn(ejercicio2, add_matrices_receive, [1, length(Mat1), [[], [], []]])),
     
-    
-    
-    % Sequential function call
-    %add_rows(list_to_tuple(lists:nth(1, Mat1)), list_to_tuple(lists:nth(1, Mat2))).
+    % Spawn sums for all rows
+    spawn_sums(1, Mat1, Mat2).
 
-    % Threads for adding rows
-    spawn(ejercicio2, add_rows, [1, list_to_tuple(lists:nth(1, Mat1)), list_to_tuple(lists:nth(1, Mat2)), self()]),
-    spawn(ejercicio2, add_rows, [2, list_to_tuple(lists:nth(2, Mat1)), list_to_tuple(lists:nth(2, Mat2)), self()]),
-    spawn(ejercicio2, add_rows, [3, list_to_tuple(lists:nth(3, Mat1)), list_to_tuple(lists:nth(3, Mat2)), self()]),
-    
-    receive
-         {added1, SumResult} ->
-            io:format("~p~n", [SumResult]);
-         
-         {added2, SumResult} ->
-            io:format("~p~n", [SumResult]);
-        
-         {added3, SumResult} ->
-            io:format("~p~n", [SumResult])
-    end,
-    
-    receive
-         {added1, SumResult2} ->
-            io:format("~p~n", [SumResult2]);
-         
-         {added2, SumResult2} ->
-            io:format("~p~n", [SumResult2]);
-        
-         {added3, SumResult2} ->
-            io:format("~p~n", [SumResult2])
-    end,
-    
-    receive
-         {added1, SumResult3} ->
-            io:format("~p~n", [SumResult3]);
-         
-         {added2, SumResult3} ->
-            io:format("~p~n", [SumResult3]);
-        
-         {added3, SumResult3} ->
-            io:format("~p~n", [SumResult3])
-    end.
-    
-    
+% Function for spawing sums required recursively
+spawn_sums(Current_Row, Mat1, Mat2) ->
+    spawn(ejercicio2, add_rows, [Current_Row, list_to_tuple(lists:nth(Current_Row, Mat1)), list_to_tuple(lists:nth(Current_Row, Mat2))]),
 
-% Function to spawn threads for calculating the sum of each row in the matrix
-add_matrices_spawn(Mat1, Mat2) ->
-
-    % Threads for adding rows
-    spawn(ejercicio2, add_rows, [1, list_to_tuple(lists:nth(1, Mat1)), list_to_tuple(lists:nth(1, Mat2)), self()]),
-    spawn(ejercicio2, add_rows, [2, list_to_tuple(lists:nth(2, Mat1)), list_to_tuple(lists:nth(2, Mat2)), self()]),
-    spawn(ejercicio2, add_rows, [3, list_to_tuple(lists:nth(3, Mat1)), list_to_tuple(lists:nth(3, Mat2)), self()]).
-
-    
-% Function to receive
-add_matrices_receive() ->
-
-    receive
-         {added1, SumResult} ->
-            io:format("~p~n", [SumResult]);
-         
-         {added2, SumResult} ->
-            io:format("~p~n", [SumResult]);
-        
-         {added3, SumResult} ->
-            io:format("~p~n", [SumResult])
-    end,
-    
-    receive
-         {added1, SumResult2} ->
-            io:format("~p~n", [SumResult2]);
-         
-         {added2, SumResult2} ->
-            io:format("~p~n", [SumResult2]);
-        
-         {added3, SumResult2} ->
-            io:format("~p~n", [SumResult2])
-    end,
-    
-    receive
-         {added1, SumResult3} ->
-            io:format("~p~n", [SumResult3]);
-         
-         {added2, SumResult3} ->
-            io:format("~p~n", [SumResult3]);
-        
-         {added3, SumResult3} ->
-            io:format("~p~n", [SumResult3])
+    if 
+        Current_Row < length(Mat1) ->
+            spawn_sums(Current_Row+1, Mat1, Mat2);
+        true->
+            ok
     end.
 
     
+% Function to receive sum results from threads
+add_matrices_receive(Sum_number, Sums_to_complete, Partial_result) ->
+
+    receive
+         {added, SumResult, RowNumber} ->
+            %ok
+            %io:format("~p~n", [SumResult]),
+            
+            % Continue receiving sums if there are any pending
+            if
+                Sum_number < Sums_to_complete ->
+                    io:format("~p~n", [Partial_result]),
+                    
+                    % Save partial result and keep receiving the other sums
+                    add_matrices_receive(Sum_number+1, Sums_to_complete, lists:sublist(Partial_result,RowNumber-1) ++ [SumResult] ++ lists:nthtail(RowNumber,Partial_result));
+
+            % TODO Once all of the sums have been completed do a matrix multiplication of the result with itself and unregister the process
+                true ->
+                    %unregister(receive_sum)
+                    ok
+            end
+    end.
     
-% Function for adding up two rows
-% add_rows(Row1, Row2) ->
-%    lists:zipwith(fun(X, Y) -> X+Y end, tuple_to_list(Row1), tuple_to_list(Row2)).
-    
+
 
 % Function for adding up two rows send result back to parent process
-add_rows(RowNumber, Row1, Row2, Parent_PID) ->
-    
-    case RowNumber of 
-        1 -> Parent_PID ! {(added1), lists:zipwith(fun(X, Y) -> X+Y end, tuple_to_list(Row1), tuple_to_list(Row2))};
-        2 -> Parent_PID ! {(added2), lists:zipwith(fun(X, Y) -> X+Y end, tuple_to_list(Row1), tuple_to_list(Row2))};
-        3 -> Parent_PID ! {(added3), lists:zipwith(fun(X, Y) -> X+Y end, tuple_to_list(Row1), tuple_to_list(Row2))}
-    end.
+add_rows(RowNumber, Row1, Row2) ->
+    receive_sum ! {(added), lists:zipwith(fun(X, Y) -> X+Y end, tuple_to_list(Row1), tuple_to_list(Row2)), RowNumber}.
 
-    
-    
 start() ->
-    %lists:zipwith(fun(X, Y) -> X+Y end, [1,2,3], [11,12,13]).
-    %spawn(ejercicio2, add_rows, [{1,2,3}, {3,2,1}]),
-    
-    % add_rows({1,2,3}, {3,2,10}).
-    
-    %spawn(ejercicio2, add_matrices, [
-    %                                 [[1,2,3],
-    %                                  [4,5,6], 
-    %                                  [7,8,9]],
 
-    %                                 [[1,2,4],
-    %                                  [4,5,6], 
-    %                                  [7,8,9]]
-    %                                          ]),
-    
     add_matrices( 
                     [[1,2,3],
-                     [1,2,3], 
+                     [4,5,6], 
                      [7,8,9]],
 
-                    [[1,2,4],
-                     [8,3,5], 
-                     [7,8,9]]
+                    [[10,11,12],
+                     [13,14,15], 
+                     [16,17,18]]
                      ).
-    
