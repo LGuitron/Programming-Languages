379b304b5c97e282aa1ff5432be9dc58ad146fd3
Luis Guitron <legl_1995@hotmail.com>
2019-05-03 23:36:46 -0500

    webapp with requests to prolog API

---
+++ Prolog/prologWebapp/src/app/character-filter/character-filter.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-character-filter',
+  templateUrl: './character-filter.component.html',
+  styleUrls: ['./character-filter.component.css']
+})
+export class CharacterFilterComponent implements OnInit {
+
+  characters : Array<any>;
+  noResults : boolean;
+
+  constructor(private http: HttpClient) { }
+
+  // Get all characters from DB
+  ngOnInit() {
+    this.noResults = false;
+  }
+
+  searchCharacters()
+  {
+
+    var gender : any = document.getElementById("gender");
+    gender = gender.options[ gender.selectedIndex ].value;
+
+    var age : any = document.getElementById("age");
+    age = age.options[ age.selectedIndex ].value;
+
+    var hair : any = document.getElementById("hair");
+    hair = hair.options[ hair.selectedIndex ].value;
+
+    let request_body = {}
+    if (String(gender) != "any")
+        request_body["gender"] = gender;
+
+    if (String(age) != "any")
+        request_body["age"] = age;
+
+    if (String(hair) != "any")
+        request_body["hair"] = hair;
+
+    this.http.post("http://localhost:4200/api", request_body).subscribe((response: any)=>{
+        this.characters = response["answer"];
+        this.noResults = this.characters.length == 0
+    });
+  }
+}
